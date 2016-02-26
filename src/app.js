cb5ab0b15ff4c9be64965e9b5452684915e7b316
Lars Miesner <larsauswsw@gmail.com>
2016-02-26 11:24:09 +0100

    Configuration

--- src/app.js
+++ src/app.js
@@ -5,8 +5,29 @@
  */
 
 Pebble.addEventListener('showConfiguration', function(e) {
-  // Show config page
-  Pebble.openURL('http://www.lars.is/pebble');
+  var url = 'https://www.lars.is/pebble/index.html';
+  console.log('Showing configuration page: ' + url);
+  Pebble.openURL(url);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  // Decode and parse config data as JSON
+  var config_data = JSON.parse(decodeURIComponent(e.response));
+  console.log('Config window returned: ', JSON.stringify(config_data));
+
+  // Prepare AppMessage payload
+  var dict = {
+    'KEY_ANIMATIONS': config_data[animSetting],
+    'KEY_TICK': config_data[tickSetting],
+    'KEY_BACKGROUND_COLOR': config_data[bgColor]
+  };
+
+  // Send settings to Pebble watchapp
+  Pebble.sendAppMessage(dict, function(){
+    console.log('Sent config data to Pebble');  
+  }, function() {
+    console.log('Failed to send config data!');
+  });
 });
 
 var UI = require('ui');
